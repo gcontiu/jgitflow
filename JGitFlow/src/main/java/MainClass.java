fe6abeb6fe965d4a87de4b91838d2e657c358996
Anghel Contiu <anghel.contiu@endava.com>
2015-10-07 15:27:59 +0300

    feature/feat - added print line

--- JGitFlow/src/main/java/MainClass.java
+++ JGitFlow/src/main/java/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Project started.");
+        System.out.println("Line added by: feature/feat");
     }
 
 }
